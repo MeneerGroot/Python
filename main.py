ee23dbe3ccad112f9964c178fead9324facedca9
meneergroot <meneergroot@protonmail.com>
2021-05-26 21:36:09 +0200

    days_to_units

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ name_of_unit = "hours"
 
 def days_to_units(num_of_days):
     print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
-    
-days_to_units(10)
+
+days_to_units(10) 
