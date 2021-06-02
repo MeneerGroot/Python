44ab22787f6730ae077789cc3a7a002b657a0f4e
meneergroot <meneergroot@protonmail.com>
2021-06-03 00:00:21 +0200

    .

---
+++ sayhello.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class SayHello(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        #add widgets to window
+
+        #add image
+        self.window.add_widget(Image(source="logo.png"))
+        #label widget
+        self.greeting = Label(text="What's your name?")
+
+        return self.window
+
+if __name__ == "__main__":
+    SayHello().run()
